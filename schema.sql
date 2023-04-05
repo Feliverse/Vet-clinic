b3447050a9e58426fe8262b4ecfb2a32eb96f72d
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-04-05 10:59:06 -0400

    Create table animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
-);
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name character varying(20),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weigth_kg numeric,
+    PRIMARY KEY (id)
+)
