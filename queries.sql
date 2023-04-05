80bb60cb3216521ef77b58688b9b4aec3282cecc
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-04-05 12:27:48 -0400

    update SQL

--- queries.sql
+++ queries.sql
@@ -1,11 +1,10 @@
-
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT name FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
 
-SELECT name FROM animals WHERE neutered IS TRUE AND escape_attempts<3;
+SELECT name FROM animals WHERE neutered IS TRUE AND escape_attempts < 3;
 
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
 
