210b1e4f34b1e514ec8bc08a8e0d139dc3aff7a6
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-04-05 11:55:43 -0400

    Insert data in table

--- data.sql
+++ data.sql
@@ -1,4 +1,3 @@
-/* Populate database with sample data. */
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weigth_kg) VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weigth_kg) VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
