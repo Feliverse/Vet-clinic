bc4412cbdfe60dbab0d4aeccd9a10ccbb081de9f
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-04-06 16:25:09 -0400

    Create tables owners and Species modify animals

--- schema.sql
+++ schema.sql
@@ -12,3 +12,30 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD species character varying(255);
+
+/*Vet clinic database: query multiple tables*/
+
+CREATE TABLE owners(
+id INT GENERATED ALWAYS AS IDENTITY,
+full_name VARCHAR(55),
+age INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE species(
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(55),
+PRIMARY KEY (id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT,
+  ADD CONSTRAINT fk_species
+  FOREIGN KEY (species_id)
+  REFERENCES species (id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT,
+  ADD CONSTRAINT fk_owners
+  FOREIGN KEY (owner_id)
+  REFERENCES owners (id);
