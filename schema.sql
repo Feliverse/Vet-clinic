693dbb38c5f6ac9dae2fdbdd041a7cb3704f310b
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-04-07 14:05:55 -0400

    return

--- schema.sql
+++ schema.sql
@@ -39,27 +39,3 @@ ALTER TABLE animals ADD COLUMN owner_id INT,
   ADD CONSTRAINT fk_owners
   FOREIGN KEY (owner_id)
   REFERENCES owners (id);
-
-/*Vet clinic database: add "join table" for visits*/
-
-CREATE TABLE vets(
-id INT GENERATED ALWAYS AS IDENTITY,
-name VARCHAR(55),
-age INT,
-date_of_graduation DATE
-);
-
-CREATE TABLE specializations (
-    species_id INT,
-    vets_id INT,
-  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id) ON DELETE CASCADE,
-  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id) ON DELETE CASCADE
-);
-
-CREATE TABLE visits (
-    animals_id INT,
-    vets_id INT,
-  date DATE,
-  CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
-  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
-);
